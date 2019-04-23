a0028d8787bb055c643ae2baaab3e0664c3619f3
yukpiz <yukpiz@gmail.com>
2019-04-23 11:19:48 +0900

    updated

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/BurntSushi/toml"
@@ -8,13 +9,20 @@ import (
 	"golang.org/x/text/language"
 )
 
+var bundle *i18n.Bundle
+
 func main() {
-	bundle := &i18n.Bundle{DefaultLanguage: language.Japanese}
+	bundle = &i18n.Bundle{DefaultLanguage: language.Japanese}
 	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
 	bundle.MustLoadMessageFile("ja-JP.toml")
 
 	localizer := i18n.NewLocalizer(bundle, "ja-JP")
 	fmt.Printf("%+v\n", localizer)
 
+	// Hello go-i18n
 	fmt.Println(localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "TestMessage"}))
+	fmt.Println(localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "Example1.Hello1"}))
+
+	err := errors.New("custom exception!")
+	localize.T("Example1.Hello1")
 }
